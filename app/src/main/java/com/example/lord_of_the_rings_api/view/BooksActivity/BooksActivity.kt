561ed7bc7a5a1c30074585aea6736c86566b38d0
fermetin <fatihermetin@gmail.com>
2020-07-07 19:23:58 +0300

    Minor Fix

--- app/src/main/java/com/example/lord_of_the_rings_api/view/BooksActivity/BooksActivity.kt
+++ app/src/main/java/com/example/lord_of_the_rings_api/view/BooksActivity/BooksActivity.kt
@@ -3,10 +3,17 @@ package com.example.lord_of_the_rings_api.view.BooksActivity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.lord_of_the_rings_api.R
+import com.example.lord_of_the_rings_api.view.BaseActivity
+import com.example.lord_of_the_rings_api.viewModel.BooksViewModel
 
-class BooksActivity : AppCompatActivity() {
+class BooksActivity : BaseActivity() {
+    private lateinit var bookViewModel : BooksViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_books)
+
+        bookViewModel = BooksViewModel()
+        
     }
 }
