5a26b628f8bee9e13d18301bd3df2cbc0b5b5997
fermetin <fatihermetin@gmail.com>
2020-07-12 23:45:52 +0300

    Bottom Tab Bar Implemented

--- app/src/main/java/com/example/lord_of_the_rings_api/view/ui/HomeActivity.kt
+++ app/src/main/java/com/example/lord_of_the_rings_api/view/ui/HomeActivity.kt
@@ -5,14 +5,26 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.lord_of_the_rings_api.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_home.*
 
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navController = findNavController(R.id.fragment)
+
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.moviesListFragment,R.id.charactersActivity,R.id.booksListFragment))
+        setupActionBarWithNavController(navController,appBarConfiguration)
+        bottomNavigationView.setupWithNavController(navController)
     }
 
 
