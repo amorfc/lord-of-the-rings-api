61d292284995add09f9292eccc2449113500c3eb
fermetin <fatihermetin@gmail.com>
2020-07-20 17:58:32 +0300

    Details Implemented

--- app/src/main/java/com/example/lord_of_the_rings_api/view/ui/movies/MovieDetailsFragment.kt
+++ app/src/main/java/com/example/lord_of_the_rings_api/view/ui/movies/MovieDetailsFragment.kt
@@ -11,6 +11,7 @@ import com.example.lord_of_the_rings_api.R
 import com.example.lord_of_the_rings_api.databinding.MovieDetailsFragmentBinding
 import com.example.lord_of_the_rings_api.viewModel.movie.MovieDetailsViewModel
 import com.example.lord_of_the_rings_api.viewModel.movie.MovieDetailsViewModelFactory
+import kotlinx.android.synthetic.main.movie_details_fragment.*
 
 class MovieDetailsFragment : Fragment() {
 
@@ -32,7 +33,24 @@ class MovieDetailsFragment : Fragment() {
         movieDetailsViewModelFactory = MovieDetailsViewModelFactory(MoviesListFragmentArgs.fromBundle(requireArguments()).selectedMovie)
 
         detailsViewModel = ViewModelProvider(this,movieDetailsViewModelFactory).get(MovieDetailsViewModel::class.java)
+        setPhoto(detailsViewModel.selectedMovie.name)
         movieDetailsFragmentBinding.movieDetailsViewModel = detailsViewModel
+    }
+
+    private fun setPhoto(name:String){
+        with(name){
+            when{
+                contains("Lord")-> imageView.setImageResource(R.drawable.rings_series)
+                contains("Hobbit")-> imageView.setImageResource(R.drawable.return_of_the_king)
+                contains("Unexpected")-> imageView.setImageResource(R.drawable.unexpected)
+                contains("Desolation")-> imageView.setImageResource(R.drawable.smaug)
+                contains("Battle")-> imageView.setImageResource(R.drawable.fivearmies)
+                contains("Two")-> imageView.setImageResource(R.drawable.twotowers)
+                contains("Fellowship")-> imageView.setImageResource(R.drawable.fellowship)
+                contains("Return")-> imageView.setImageResource(R.drawable.return_of_the_king)
+            }
+
+        }
     }
 
 }
