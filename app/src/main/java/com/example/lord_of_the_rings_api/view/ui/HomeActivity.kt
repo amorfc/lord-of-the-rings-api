f0a1e30b2887b77f52f0b16ceab737fb6c325e07
fermetin <fatihermetin@gmail.com>
2020-07-21 17:03:20 +0300

    MinorFix

--- app/src/main/java/com/example/lord_of_the_rings_api/view/ui/HomeActivity.kt
+++ app/src/main/java/com/example/lord_of_the_rings_api/view/ui/HomeActivity.kt
@@ -2,7 +2,10 @@ package com.example.lord_of_the_rings_api.view.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
@@ -12,18 +15,18 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
 
+        private val controller : LiveData<NavController>? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        val navController = findNavController(R.id.navHostFragment)
+        val navController =findNavController(R.id.navHostFragment)
 
         setupActionBarWithNavController(navController)
 
         findViewById<BottomNavigationView>(R.id.bottomNavigationView)
             .setupWithNavController(navController)
-
-
     }
 
 
